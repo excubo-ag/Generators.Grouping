9c5ed28d83ab7a05c0e7169b4b70a34dbc945e48
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-08-21 10:15:28 +0200

    add integration test (this exposes a bug: the razor component prevents correct behavior of the SG!)

---
+++ IntegrationTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Excubo.Generators.Grouping;
+using System;
+
+namespace IntegrationTest
+{
+    public partial class Class1
+    {
+        public partial struct _Group1 
+        {
+            public partial struct _SubGroup { }
+        }
+        [Group(typeof(_Group1))] public void Foo() { Console.WriteLine(nameof(Foo)); }
+        [Group(typeof(_Group1._SubGroup))] public void NestedFoo() { Console.WriteLine(nameof(NestedFoo)); }
+        public void Consumption()
+        {
+            Foo();
+            Group1.Foo();
+            NestedFoo();
+            Group1.SubGroup.NestedFoo();
+        }
+    }
+}
